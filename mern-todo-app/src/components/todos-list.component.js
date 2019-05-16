d2b1907f16975d56042f9b385eb4f5eef2a9af53
Anatoliy Frolov <mazzzfaka@gmail.com>
2019-05-16 12:08:59 +0300

    На этом моменте начал созодвать компоненты

---
+++ mern-todo-app/src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+            <p>Привет в компонете всего списка дел</p>
+        </div>
+    )
+    }
+}
