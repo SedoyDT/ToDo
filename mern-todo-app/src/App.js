b71000b8cd2c5dbb29c35a9a3485b60a98e69a14
Anatoliy Frolov <mazzzfaka@gmail.com>
2019-05-16 12:20:50 +0300

    Все работает так как должно на этом моменте.Но в какой-то момент нужно было установить react-router-dom

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -6,7 +6,6 @@ import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
-import logo from "./logo.png";
 
 class App extends Component {
     render() {
@@ -16,7 +15,7 @@ class App extends Component {
 
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-                            <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+
                         </a>
                         <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
                         <div className="collpase nav-collapse">
